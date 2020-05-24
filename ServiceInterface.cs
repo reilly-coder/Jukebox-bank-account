c8b1526e0a939e06a1338cd091901ade73f534a6
Niall <niall.mckeown@yahoo.com>
2020-05-25 00:25:34 +0100

    Initial commit for media service spike

--- ServiceInterface.cs
+++ ServiceInterface.cs
@@ -8,6 +8,12 @@ namespace HNDunit20
 {
     interface ServiceInterface
     {
-        void performOperation();
+        double SongPrice { get; }
+
+        IList<string> Songs { get; }
+
+        void ListAll();
+
+        string Purchase(MediaAccount account, string songToPurchase);
     }
 }
